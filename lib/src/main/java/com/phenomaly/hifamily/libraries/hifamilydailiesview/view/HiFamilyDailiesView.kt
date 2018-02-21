3e57c515d0088efee7a8c06916f0e3e15eba643e
Andrzej Ryl <a.ryl@phenoma.ly>
2018-02-21 09:55:26 +0100

    Add listeners that are going to be needed in analytics

--- lib/src/main/java/com/phenomaly/hifamily/libraries/hifamilydailiesview/view/HiFamilyDailiesView.kt
+++ lib/src/main/java/com/phenomaly/hifamily/libraries/hifamilydailiesview/view/HiFamilyDailiesView.kt
@@ -11,6 +11,13 @@ import com.phenomaly.hifamily.libraries.hifamilydailiesview.getColor
 import com.phenomaly.hifamily.libraries.hifamilydailiesview.getDimension
 import com.phenomaly.hifamily.libraries.hifamilydailiesview.getScreenWidth
 
+enum class DailyElement {
+    CAROUSEL, DAILIES
+}
+
+typealias OnDailyElementTouchedListener = (element: DailyElement) -> Unit
+typealias DailySelectedListener = (index: Int) -> Unit
+
 class HiFamilyDailiesView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
@@ -31,6 +38,11 @@ class HiFamilyDailiesView @JvmOverloads constructor(
 
     private lateinit var carouselAdapter: CarouselAdapter
     private lateinit var carouselOnScrolledListener: OnScrolledListener
+
+    private val dailySelectedListeners = arrayListOf<DailySelectedListener>()
+    private val onTouchListeners = arrayListOf<OnDailyElementTouchedListener>()
+
+    private var currentDailyElement = DailyElement.CAROUSEL
 
     init {
         inflate(context, R.layout.view_hi_family_dailies, this)
@@ -77,6 +89,24 @@ class HiFamilyDailiesView @JvmOverloads constructor(
         additionalViewContainer.removeAllViews()
     }
 
+    fun addOnDailySelectedListener(listener: DailySelectedListener) {
+        dailySelectedListeners.add(listener)
+    }
+
+    fun clearDailySelectedListeners() {
+        dailySelectedListeners.clear()
+    }
+
+    fun getCurrentDailyIndex() = dailiesViewPager.currentItem
+
+    fun addOnTouchListener(listener: OnDailyElementTouchedListener) {
+        onTouchListeners.add(listener)
+    }
+
+    fun clearOnTouchListeners() {
+        onTouchListeners.clear()
+    }
+
     private fun initDailiesAdapter(availableDailies: Map<String, String>) {
         dailiesAdapter = DailiesAdapter(
                 context,
@@ -89,9 +119,10 @@ class HiFamilyDailiesView @JvmOverloads constructor(
         )
 
         dailiesViewPager.adapter = dailiesAdapter
-        dailiesPageListener = {
+        dailiesPageListener = { position ->
+            dailySelectedListeners.forEach { it.invoke(position) }
             carouselScrollView
-                    .smoothScrollToPosition(it)
+                    .smoothScrollToPosition(position)
         }
     }
 
@@ -109,7 +140,10 @@ class HiFamilyDailiesView @JvmOverloads constructor(
         )
 
         carouselScrollView.adapter = carouselAdapter
-        carouselOnScrolledListener = { dailiesViewPager.setCurrentItem(it, false) }
+        carouselOnScrolledListener = { position ->
+            dailySelectedListeners.forEach { it.invoke(position) }
+            dailiesViewPager.setCurrentItem(position, false)
+        }
         carouselScrollView.addCustomScrollListener(carouselOnScrolledListener)
     }
 
@@ -123,14 +157,24 @@ class HiFamilyDailiesView @JvmOverloads constructor(
     }
 
     private fun onDailiesTouched(): Boolean {
-        carouselScrollView.clearOnScrollListeners()
-        dailiesViewPager.addCustomOnPageSelectedListener(dailiesPageListener)
+        if (currentDailyElement == DailyElement.CAROUSEL) {
+            currentDailyElement = DailyElement.DAILIES
+            onTouchListeners.forEach { it.invoke(DailyElement.DAILIES) }
+
+            carouselScrollView.clearOnScrollListeners()
+            dailiesViewPager.addCustomOnPageSelectedListener(dailiesPageListener)
+        }
         return false
     }
 
     private fun onCarouselTouched(): Boolean {
-        dailiesViewPager.clearCustomOnPageSelectedListeners()
-        carouselScrollView.addCustomScrollListener(carouselOnScrolledListener)
+        if (currentDailyElement == DailyElement.DAILIES) {
+            currentDailyElement = DailyElement.CAROUSEL
+            onTouchListeners.forEach { it.invoke(DailyElement.CAROUSEL) }
+
+            dailiesViewPager.clearCustomOnPageSelectedListeners()
+            carouselScrollView.addCustomScrollListener(carouselOnScrolledListener)
+        }
         return false
     }
 }
